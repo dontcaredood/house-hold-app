c2f3b477a634dce2f00b7568974800a503e1c43a
dontcaredood <65106249+dontcaredood@users.noreply.github.com>
2024-08-09 11:09:34 +0530

    Added the tags for better documentation.

--- src/main/java/com/household/code/controller/ProductsController.java
+++ src/main/java/com/household/code/controller/ProductsController.java
@@ -6,6 +6,8 @@ import com.household.code.model.GroceriesResponse;
 import com.household.code.model.ToiletriesResponse;
 import com.household.code.service.GroceriesService;
 import com.household.code.service.ToiletriesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,27 +22,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/household/product")
 @Log4j2
+@Tag(name = "Products", description = "API for managing household products including groceries and toiletries.")
 public class ProductsController {
+
     @Autowired
     GroceriesService groceriesService;
+
     @Autowired
     ToiletriesService toiletriesService;
 
     @GetMapping("/hi/{userName}")
+    @Operation(summary = "Greet the user", description = "Returns a greeting message for the user.")
     public String sayHi(@PathVariable String userName) {
         return "hi " + userName + ". Welcome to HouseHold application.";
     }
 
     @GetMapping("/groceries")
+    @Operation(summary = "Get all groceries", description = "Returns a list of all groceries with their details.")
     public ResponseEntity<GroceriesResponse> getGroceries() {
         try {
             List<Groceries> allGroceries = groceriesService.getAllGroceries();
             GroceriesResponse groceriesResponse = new GroceriesResponse();
-
-//            GroceriesResponse groceriesResponse = GroceriesResponse.builder()
-//                    .noOfProducts((long) allGroceries.size())
-//                    .groceriesList(allGroceries)
-//                    .build();
 
             groceriesResponse.setGroceriesList(allGroceries);
             groceriesResponse.setNoOfProducts((long) allGroceries.size());
@@ -56,21 +58,19 @@ public class ProductsController {
     }
 
     @GetMapping("/toileteries")
-    public ResponseEntity<ToiletriesResponse> getToiletries(){
-
-        try{
+    @Operation(summary = "Get all toiletries", description = "Returns a list of all toiletries with their details.")
+    public ResponseEntity<ToiletriesResponse> getToiletries() {
+        try {
             List<Toiletries> allToiletries = toiletriesService.getAllToiletries();
             ToiletriesResponse toiletriesResponse = new ToiletriesResponse();
             toiletriesResponse.setNoOfProducts((long) allToiletries.size());
             toiletriesResponse.setToiletriesList(allToiletries);
             log.info("ToiletriesResponse: {}", toiletriesResponse);
-            if (allToiletries.isEmpty()){
+            if (allToiletries.isEmpty()) {
                 return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
             }
-            return  new ResponseEntity<>(toiletriesResponse, HttpStatus.OK);
-
-        }
-        catch (Exception exception){
+            return new ResponseEntity<>(toiletriesResponse, HttpStatus.OK);
+        } catch (Exception exception) {
             log.error("Internal Server Error: {}", exception.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
